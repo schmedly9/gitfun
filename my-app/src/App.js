9b76db131deacbc45e0f7fae6fb372b6d35048cd
Bob Schneider <schmedly99@gmail.com>
2018-06-03 11:55:23 -0700

    c

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,36 +11,36 @@ import './App.css';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       cards:[],
       score: 0,
       highscore: 0
 
-    };
+    }
 
   gameOver = () => {
     if (this.state.score > this.state.highscore) {
       this.setState({highscore: this.state.score}, function() {
-        console.log(this.state.highscore);
-      });
+        console.log(this.state.highscore)
+      })
     }
     this.state.cards.forEach(card => {
-      card.count = 0;
-    });
-    alert(`Game Over :( \nscore: ${this.state.score}`);
-    this.setState({score: 0});
-    return true;
+      card.count = 0
+    })
+    alert(`Game Over :( \nscore: ${this.state.score}`)
+    this.setState({score: 0})
+    return true
   }
 
   clickCount = id => {
     this.state.cards.find((o, i) => {
       if (o.id === id) {
         if(cards[i].count === 0){
-          cards[i].count = cards[i].count + 1;
+          cards[i].count = cards[i].count + 1
           this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
-          });
+            console.log(this.state.score)
+          })
           this.state.cards.sort(() => Math.random() - 0.5)
           return true; 
         } else {
@@ -52,19 +52,20 @@ class App extends React.Component {
       return (
       <div className = "Wrapper" className = "Header" score={this.state.score} 
             highscore={this.state.highscore}
-            {this.state.cards.map(cards[]) = 
+            this.state.cards.map(cards[]) =
            <cards
             clickCount={this.clickCount}
             id={card.id}
             key={card.id}
             image={card.image}
-        /> 
-//       )
-//    )
-    });
+          />  
+            
+          
+    
   }
-}
-}
+    
+
+
 
    
     
